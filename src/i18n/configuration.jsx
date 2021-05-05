e057006ae1e5f901073780d32df659ae5b7dbf66
YuvalPeretz <yuvalperetzj@gmail.com>
2021-05-05 22:21:09 +0300

    added js-cookies and implemented

--- src/i18n/configuration.jsx
+++ src/i18n/configuration.jsx
@@ -2,6 +2,13 @@ import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
+import cookies from "js-cookie"
+
+(function currentDir() {
+    if (cookies.get("i18next") === "en" || !cookies.get("i18next"))
+        document.dir = "ltr";   
+    else document.dir = "rtl"
+}())
 
 i18next
     .use(initReactI18next)
